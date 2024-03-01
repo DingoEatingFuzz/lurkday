176f32c79e15f0f9e0076d19b3f3c126e9e7f326
Michael Lange <dingoeatingfuzz@gmail.com>
2024-03-01 00:23:01 -0800

    Args parsing

--- index.ts
+++ index.ts
@@ -1,1 +1,29 @@
-console.log("Hello via Bun!");
+// This is a bun import, dunno if it make things be bad in node
+import { parseArgs } from 'util';
+import { createHash } from 'crypto';
+
+const args = parseArgs({
+  allowPositionals: true,
+  options: {
+    'no-cache': { type: 'boolean' },
+  }
+});
+
+const NO_CACHE = args.values['no-cache'];
+const FILE = args.positionals[0];
+
+console.log('File', NO_CACHE, FILE, checksum(FILE));
+
+// Take in an excel sheet
+// Convert into json records
+// Cache records in a local file
+// Construct a tree with it
+// Start repl
+
+
+function checksum(digest: string): string {
+  return createHash('sha256')
+    .update(digest)
+    .digest()
+    .toString('hex')
+}
